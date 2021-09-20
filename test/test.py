72a640523dabe14486d496f91942caf855e85558
Erik <erik.pfannstiel@hsu-hh.de>
2021-09-20 17:14:09 +0200

    Added GNUGPLv3 License

--- test/test.py
+++ test/test.py
@@ -9,7 +9,7 @@ def main():
     w = QWidget()
     w.resize(250, 150)
     w.move(300, 300)
-    w.setWindowTitle('Simple')
+    w.setWindowTitle('Test PyQt')
     w.show()
 
     sys.exit(app.exec_())
